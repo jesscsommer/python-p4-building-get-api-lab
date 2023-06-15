9a9a31533a0daed4068aecfd6606c7389be2ddd2
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-14 17:51:23 -0700

    Complete lab

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,27 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(bakeries, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    if b := db.session.get(Bakery, id):
+        return make_response(b.to_dict(), 200)
+    else: 
+        response_body = f"404 Not Found: No bakery with id {id}"
+        return make_response(response_body, 404)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    sorted_goods = [bg.to_dict() for bg 
+                    in BakedGood.query.order_by(desc(BakedGood.price)).all()]
+    return make_response(sorted_goods, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    return make_response(most_expensive.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
